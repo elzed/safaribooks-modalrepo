f9d3353fced163da72bcf7da761a6bfeb0878c06
elzed <elzed@users.noreply.github.com>
2018-08-10 10:02:15 -0700

    using const keyword and added comment

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 document.addEventListener("mouseup", function() {
-    var element = document.getElementsByClassName("annotator-adder");
-    var topValue = element[0].style.top;
+
+    const element = document.getElementsByClassName("annotator-adder");
+    const topValue = element[0].style.top;
+
     console.log("Property 'top' value: " + topValue);
     console.log("Type: " + (typeof topValue));
-    var numberify = parseFloat(topValue);
-    var newTopValue = ((numberify - 100) + "px");
+
+    const floatVal = parseFloat(topValue);
+    const newTopValue = ((floatVal - 100) + "px");
+
     console.log("Recalculated 'top' value: " + newTopValue);
+
     element[0].style.top = newTopValue;
+
 });
 
-// calculate the number to subtract based on the window height
+// calculate the subtrahend based on browser window height
+// use window.innerHeight
