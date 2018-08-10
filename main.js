095bcaca5941c73841156ecaab8e7be0d389c7a7
elzed <elzed@users.noreply.github.com>
2018-08-10 09:19:07 -0700

    parsed resulting string to remove , calculate, then add  back

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 document.addEventListener("mouseup", function() {
-    console.log("Mouse Upped!");
     var element = document.getElementsByClassName("annotator-adder");
     var topValue = element[0].style.top;
-    console.log(topValue);
-    var style = window.getComputedStyle(element);
-    var foundTop = style.getPropertyValue("top");
-    console.log(foundTop);
+    console.log("Property 'top' value: " + topValue);
+    console.log("Type: " + (typeof topValue));
+    var numberify = parseFloat(topValue);
+    var newTopValue = ((numberify - 100) + "px");
+    console.log("Recalculated 'top' value: " + newTopValue);
+    element[0].style.top = newTopValue;
 });
+
+// calculate the number to subtract based on the window height
